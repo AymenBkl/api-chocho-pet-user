180993bf84f29823e7beb928be2c922b240a5461
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-06-17 03:47:35 +0200

    ready to push

--- Controllers/Logger/productLogger.js
+++ Controllers/Logger/productLogger.js
@@ -8,21 +8,3 @@ module.exports.insertProductLogger = (object) => {
         })
 }
 
-module.exports.getProductLogger = (res,level) => {
-    productLogger.find({level:level})
-        .then((loggers) => {
-            if (loggers && loggers.length > 0){
-                res.json({status:200,success:true,object:loggers});
-            }
-            else if (loggers && loggers.length == 0){
-                res.json({status:404,success:true,object:loggers});
-            }
-            else {
-                res.json({status:500,success:false,err:'Somthing Went Wrong !'});
-            }
-        })
-        .catch(err => {
-            res.json({status:500,success:false,err:err});
-        })
-}
-
